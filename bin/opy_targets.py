7195e51da88dc8eef9713fdd139cbb325ecee062
toozig <toozig@gmail.com>
2023-08-24 15:48:40 +0300

    added get variants functions1

--- bin/opy_targets.py
+++ bin/opy_targets.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 import json
+from multiprocessing import Pool
 
 DISEASE_GENE_QUERY = """
         query DiseaseAssociationsQuery($efoId: String!, $index: Int!, $size: Int!, $filter: String, $sortBy: String!, $aggregationFilters: [AggregationFilter!]) {
@@ -78,6 +79,41 @@ SNP_DATA_QUERY = """
         }
 
         """
+
+VARIANT_QUERY= """
+    query variantInfo($variantId: String!) {
+        variantInfo(variantId: $variantId) {
+            id
+            rsId
+            chromosome
+            position
+            refAllele
+            altAllele
+            nearestGene{
+                symbol
+            }
+            nearestGeneDistance
+            nearestCodingGeneDistance
+            mostSevereConsequence
+            caddRaw
+            caddPhred
+            gnomadAFR
+            gnomadAMR
+            # ... (other gnomad population fields)
+        }
+    }
+    """
+    
+
+VARIANT_ID_QUERY = """
+    query searchRsId($rsId: String!) {
+        search(queryString: $rsId) {
+            variants {
+                id
+            }
+        }
+    }
+    """
 
 
 def flatten_json(y):
@@ -205,3 +241,108 @@ def get_SNP_df(efoId: str, n_gene:int =25, sort_by: str='genetic_association', s
     for snp_id in disease_df['id'][:n_gene]:
         snp_result = pd.concat([snp_result, get_snp_data(snp_id,efoId)])
     return snp_result[~snp_result['variantRsId'].duplicated()].reset_index(drop=True)
+
+
+# Function to search for a variant by its rsID using the Open Targets Genetics API
+def search_variant_by_rsId(rsId):
+    """
+    Searches for a variant by its rsID using the Open Targets Genetics API.
+
+    Args:
+        rsId (str): The rsID of the variant to search for.
+
+    Returns:
+        str: The ID of the found variant or an empty string if not found.
+    """
+    url = 'https://api.genetics.opentargets.org/graphql'
+    
+
+    
+    variables = {'rsId': rsId}
+    
+    search_result = requests.post(url, json={'query': VARIANT_ID_QUERY, 'variables': variables}).json()
+    variants = search_result.get('data', {}).get('search', {}).get('variants', [])
+    
+    if variants:
+        variant_id = variants[0]['id']
+        return variant_id
+    else:
+        return ""
+
+# Function to retrieve detailed information about a variant by its variant ID
+def get_variant_info(variant_id):
+    """
+    Retrieves detailed information about a variant by its variant ID using the Open Targets Genetics API.
+
+    Args:
+        variant_id (str): The ID of the variant to retrieve information for.
+
+    Returns:
+        dict: A dictionary containing information about the variant.
+    """
+    url = 'https://api.genetics.opentargets.org/graphql'
+    
+
+    if variant_id == "":
+        return ""
+    
+    variables = {'variantId': variant_id}
+    
+    variant_result = requests.post(url, json={'query': VARIANT_QUERY, 'variables': variables}).json()
+    
+    return variant_result['data']['variantInfo']
+
+# Function to convert variant JSON data to a Pandas Series
+def var_json_to_series(variant_json):
+    """
+    Converts variant JSON data to a Pandas Series.
+
+    Args:
+        variant_json (dict): A dictionary containing variant information.
+
+    Returns:
+        pd.Series: A Pandas Series containing variant information.
+    """
+    if variant_json == "":
+        return pd.Series()
+    
+    variant = pd.Series(flatten_json(variant_json))  # Assuming flatten_json is defined elsewhere
+    gnomAD_cols = variant.index[variant.index.str.contains('gnomad')]
+    AF_popmax = variant[gnomAD_cols].max()
+    variant = variant.drop(gnomAD_cols)
+    variant['gnomAD2_AF_popmax'] = AF_popmax
+    variant = variant.rename({'chromosome': 'CHROM', 'position': 'POS', 'refAllele': 'REF', 'altAllele': 'ALT'})
+    return variant
+
+# Function to get detailed information for a single variant by its rsID
+def get_single_variant_info(rsid: str):
+    """
+    Gets detailed information for a single variant by its rsID.
+
+    Args:
+        rsid (str): The rsID of the variant.
+
+    Returns:
+        pd.Series: A Pandas Series containing detailed variant information.
+    """
+    var_id = search_variant_by_rsId(rsid)
+    if var_id:
+        var_json = get_variant_info(var_id)
+        return var_json_to_series(var_json)
+
+    
+# Function to get detailed information for multiple variants
+def get_variants_info(variant_series: pd.Series):
+    """
+    Gets detailed information for multiple variants.
+
+    Args:
+        variant_series (pd.Series): A Pandas Series containing variant rsIDs.
+
+    Returns:
+        pd.DataFrame: A Pandas DataFrame containing detailed information for the variants.
+    """
+    with Pool(processes=8) as pool:  # Adjust the 'processes' parameter based on your CPU cores
+        results = pool.map(get_single_variant_info, variant_series)
+    
+    return pd.concat(results, axis=1).T.sort_values(['CHROM', 'POS', 'REF', 'ALT']).set_index(['CHROM', 'POS', 'REF', 'ALT'])
